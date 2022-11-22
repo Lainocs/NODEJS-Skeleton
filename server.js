b411f4d38cd822ff241e620f20cd46b368041fe5
Nicolas de Garrigues <ndegarrigues@gmail.com>
2022-11-22 17:02:58 +0100

    :sparkles: FEAT add cors

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ import express from 'express'
 import router from './api/router.js'
 import bodyParser from 'body-parser'
 import database from './database.js'
+import cors from 'cors'
 
 const app = express()
-const port = 3001
+const port = 3000
+
+database.connect()
+
+app.use(cors())
 
 app.use(bodyParser.json())
 
 app.use('/', router)
 
-database.connect()
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
